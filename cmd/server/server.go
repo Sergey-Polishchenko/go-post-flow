89c88f860b98075bf25c228908ad1fa36ccfd244
Sergey <sergeypolishchenko@gmail.com>
2025-02-08 18:07:38 +0300

    feat: add GraphQL server entry point

---
+++ cmd/server/server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+
+	"github.com/Sergey-Polishchenko/go-post-flow/internal/delivery/graph/generated"
+	"github.com/Sergey-Polishchenko/go-post-flow/internal/delivery/graph/resolvers"
+	"github.com/Sergey-Polishchenko/go-post-flow/internal/repository"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	// TODO: make custom config
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	// TODO: repository selection
+	storage := repository.LoadStorage(true)
+
+	execSchema := generated.NewExecutableSchema(
+		generated.Config{
+			Resolvers: resolvers.NewResolver(storage),
+		},
+	)
+
+	srv := handler.New(execSchema)
+
+	srv.AddTransport(
+		&transport.Websocket{
+			KeepAlivePingInterval: 10 * time.Second,
+		},
+	)
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.MultipartForm{})
+
+	http.Handle("/", playground.Handler("GraphQL Playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("🚀 Server ready at http://localhost:%s/", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
