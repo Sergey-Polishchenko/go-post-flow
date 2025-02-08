72a6d0b367604970c68b329f99b97b19d19faae7
Sergey <sergeypolishchenko@gmail.com>
2025-02-08 17:46:44 +0300

    feat: add user methods to in-memory storage

--- internal/repository/in_memory/user.go
+++ internal/repository/in_memory/user.go
@@ -1,13 +1,31 @@
 package inmemory
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/Sergey-Polishchenko/go-post-flow/internal/delivery/graph/model"
 )
 
 func (s *InMemoryStorage) CreateUser(input model.UserInput) (*model.User, error) {
-	return nil, nil
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	user := &model.User{
+		ID:   fmt.Sprintf("user_%d", len(s.Users)+1),
+		Name: input.Name,
+	}
+	s.Users[user.ID] = user
+	return user, nil
 }
 
 func (s *InMemoryStorage) GetUser(id string) (*model.User, error) {
-	return nil, nil
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	user, exists := s.Users[id]
+	if !exists {
+		return nil, errors.New("user not found")
+	}
+	return user, nil
 }
