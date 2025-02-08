7953f1183cceeca6aa45d4f5ad9fe66f9dfb167f
Sergey <sergeypolishchenko@gmail.com>
2025-02-08 17:39:41 +0300

    feat: add in-memory storage

---
+++ internal/repository/in_memory/storage.go
@@ -0,0 +1,22 @@
+package inmemory
+
+import (
+	"sync"
+
+	"github.com/Sergey-Polishchenko/go-post-flow/internal/delivery/graph/model"
+)
+
+type InMemoryStorage struct {
+	mu       sync.RWMutex
+	Posts    map[string]*model.Post
+	Comments map[string]*model.Comment
+	Users    map[string]*model.User
+}
+
+func NewStorage() *InMemoryStorage {
+	return &InMemoryStorage{
+		Posts:    make(map[string]*model.Post),
+		Comments: make(map[string]*model.Comment),
+		Users:    make(map[string]*model.User),
+	}
+}
