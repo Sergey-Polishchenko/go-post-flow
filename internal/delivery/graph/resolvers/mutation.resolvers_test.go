e5ef5cfc58f0b32b2fb3dab0f5821443a28183ff
Sergey <sergeypolishchenko@gmail.com>
2025-02-08 18:53:13 +0300

    test: add mutation resolvers tests

---
+++ internal/delivery/graph/resolvers/mutation.resolvers_test.go
@@ -0,0 +1,91 @@
+package resolvers
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/Sergey-Polishchenko/go-post-flow/internal/delivery/graph/model"
+	"github.com/Sergey-Polishchenko/go-post-flow/internal/delivery/graph/resolvers/mock"
+)
+
+func Test_mutationResolver_CreatePost(t *testing.T) {
+	mockStorage := new(mock.MockStorage)
+	mockPost := &model.Post{ID: "1", Title: "New Post", Content: "This is a new post."}
+	input := model.PostInput{Title: "New Post", Content: "This is a new post."}
+
+	mockStorage.On("CreatePost", input).Return(mockPost, nil)
+
+	r := &mutationResolver{
+		Resolver: &Resolver{
+			storage: mockStorage,
+		},
+	}
+
+	ctx := context.Background()
+	got, err := r.CreatePost(ctx, input)
+	if err != nil {
+		t.Errorf("mutationResolver.CreatePost() error = %v, wantErr %v", err, false)
+		return
+	}
+
+	if !reflect.DeepEqual(got, mockPost) {
+		t.Errorf("mutationResolver.CreatePost() = %v, want %v", got, mockPost)
+	}
+
+	mockStorage.AssertCalled(t, "CreatePost", input)
+}
+
+func Test_mutationResolver_CreateComment(t *testing.T) {
+	mockStorage := new(mock.MockStorage)
+	mockComment := &model.Comment{ID: "1", Text: "New Comment"}
+	input := model.CommentInput{Text: "New Comment"}
+
+	mockStorage.On("CreateComment", input).Return(mockComment, nil)
+
+	r := &mutationResolver{
+		Resolver: &Resolver{
+			storage: mockStorage,
+		},
+	}
+
+	ctx := context.Background()
+	got, err := r.CreateComment(ctx, input)
+	if err != nil {
+		t.Errorf("mutationResolver.CreateComment() error = %v, wantErr %v", err, false)
+		return
+	}
+
+	if !reflect.DeepEqual(got, mockComment) {
+		t.Errorf("mutationResolver.CreateComment() = %v, want %v", got, mockComment)
+	}
+
+	mockStorage.AssertCalled(t, "CreateComment", input)
+}
+
+func Test_mutationResolver_CreateUser(t *testing.T) {
+	mockStorage := new(mock.MockStorage)
+	mockUser := &model.User{ID: "1", Name: "New User"}
+	input := model.UserInput{Name: "New User"}
+
+	mockStorage.On("CreateUser", input).Return(mockUser, nil)
+
+	r := &mutationResolver{
+		Resolver: &Resolver{
+			storage: mockStorage,
+		},
+	}
+
+	ctx := context.Background()
+	got, err := r.CreateUser(ctx, input)
+	if err != nil {
+		t.Errorf("mutationResolver.CreateUser() error = %v, wantErr %v", err, false)
+		return
+	}
+
+	if !reflect.DeepEqual(got, mockUser) {
+		t.Errorf("mutationResolver.CreateUser() = %v, want %v", got, mockUser)
+	}
+
+	mockStorage.AssertCalled(t, "CreateUser", input)
+}
