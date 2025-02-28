ea1b4a7c44f18e833a01fe6143d4913674f92376
Sergey <sergeypolishchenko@gmail.com>
2025-02-28 17:10:06 +0300

    feat: add replies getters methods in comment repository interface

--- internal/core/comment/repo.go
+++ internal/core/comment/repo.go
@@ -7,4 +7,6 @@ type CommentRepository interface {
 	Create(ctx context.Context, comment *Comment) error
 	Remove(ctx context.Context, id string) error
 	GetById(ctx context.Context, id string) (*Comment, error)
+	GetReplies(ctx context.Context, id string) ([]*Comment, error)
+	GetPostReplies(ctx context.Context, postId string) ([]*Comment, error)
 }
