3384b66c04dd7994fb56cd0a01f1dfe809b5aef4
Sergey <sergeypolishchenko@gmail.com>
2025-03-01 12:58:54 +0300

    feat: change comment repository interface for create method

--- internal/core/comment/repo.go
+++ internal/core/comment/repo.go
@@ -4,7 +4,7 @@ import "context"
 
 // CommentRepository defines persistence operations for Comments.
 type CommentRepository interface {
-	Create(ctx context.Context, comment *Comment) error
+	Create(ctx context.Context, authorId string, text string) (*Comment, error)
 	Remove(ctx context.Context, id string) error
 	GetById(ctx context.Context, id string) (*Comment, error)
 	GetReplies(ctx context.Context, id string) ([]*Comment, error)
