6a168a57bdad9fc990cad57b50e9e30dcb50a961
Sergey <sergeypolishchenko@gmail.com>
2025-03-01 12:59:10 +0300

    feat: change post repository interface for create method

--- internal/core/post/repo.go
+++ internal/core/post/repo.go
@@ -4,7 +4,7 @@ import "context"
 
 // PostRepository defines persistence operations for Posts.
 type PostRepository interface {
-	Create(ctx context.Context, post *Post) error
+	Create(ctx context.Context, authorId string, title string, content string) (*Post, error)
 	Remove(ctx context.Context, id string) error
 	GetById(ctx context.Context, id string) (*Post, error)
 	List(ctx context.Context, offset int, limit int) ([]*Post, error)
