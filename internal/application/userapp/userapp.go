b237e31afc61ae5efcd99693370ba51be12b14ef
Sergey <sergeypolishchenko@gmail.com>
2025-03-03 12:00:21 +0300

    feat: add UserDTOs in UserRepository interface and rename create method to save, add Logger in userapp

--- internal/application/userapp/userapp.go
+++ internal/application/userapp/userapp.go
@@ -3,20 +3,21 @@ package userapp
 import (
 	"context"
 
-	"github.com/Sergey-Polishchenko/go-post-flow/internal/core/user"
+	"github.com/Sergey-Polishchenko/go-post-flow/internal/pkg/logging"
 )
 
 // UserRepository defines persistence operations for Users.
 type UserRepository interface {
-	Create(ctx context.Context, name string) (*user.User, error)
+	Save(ctx context.Context, user UserDTO) error
 	Remove(ctx context.Context, id string) error
-	GetByID(ctx context.Context, id string) (*user.User, error)
+	GetByID(ctx context.Context, id string) (UserDTO, error)
 }
 
 type UserApp struct {
-	repo UserRepository
+	repo   UserRepository
+	logger logging.Logger
 }
 
-func New(repo UserRepository) *UserApp {
-	return &UserApp{repo: repo}
+func New(repo UserRepository, logger logging.Logger) *UserApp {
+	return &UserApp{repo: repo, logger: logger}
 }
